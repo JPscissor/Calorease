6aa65d50e01212fcd14729e34867838aed685375
JPscissor <squareguy@mail.ru>
2025-03-20 09:46:40 +0300

    working... (20.03.25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.3.0"
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
